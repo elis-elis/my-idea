e832aae87c6fb931ccfa45017f84700509713b96
elis-elis <elisprostotak@gmail.com>
2024-08-11 11:14:49 +0200

    Create primary style.css, link other css pages to it, create blog_area.html

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def goodbye_world():
-    return 'Goodbye, World... uufff, ah wait a moment...'
+@app.route("/")
+def index():
+    return render_template("index.html")
+    pass
+
+
+@app.route("/blog-area", methods=["GET"])
+def blogs():
+    return render_template("blog_area.html")
+    pass
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=5000, debug=True)
